c1fcdff26a4e6f6278b8403819098ebd72ad44e6
Pankaj Yadav <pankajyadav1600@outlook.com>
2021-02-25 16:32:08 +0530

    update

--- EmployeeInformationSystem.WebUI/Controllers/LeaveController.cs
+++ EmployeeInformationSystem.WebUI/Controllers/LeaveController.cs
@@ -430,7 +430,7 @@ namespace EmployeeInformationSystem.WebUI.Controllers
             List<EmployeeDetail> EmployeeList = EmployeeDetailContext.Collection().Where(q => q.WorkingStatus == true).OrderBy(e=>e.FirstName).ToList() ;
             ViewBag.EmlployeeTypeList = EmployeeList;
             ViewBag.LevelTypeList = null;
-           ViewBag.HeadingName = "Apply Leave";
+            ViewBag.HeadingName = "Apply Leave";
             ViewBag.HeadingColor = "bg-success";
             ViewBag.Name = "";
             return View();
@@ -442,16 +442,28 @@ namespace EmployeeInformationSystem.WebUI.Controllers
             string returnText = "Error";
             if (ModelState.IsValid)
             {
-                EmployeeLeaveDetailsContext.Insert(Eld);
-                EmployeeLeaveDetailsContext.Commit();
-                returnText = "Success";
-            }
-            if (returnText == "Success")
-            {
-                EmpLeaveBalCount(Eld);
-                ViewBag.HeadingName = "Add Leave Type";
-                ViewBag.HeadingColor = "bg-success";
-                ViewBag.Msg = "Leave Apply Succesfully";
+                var AvailableLeave = EmployeeLeaveBalanceContext.Collection().ToList().Where(aa => aa.EmployeeId == Eld.EmployeeId && aa.LeaveTypeId == Eld.LeaveTypeId).
+                    Select(aa => aa.AvailableLeaveCount).SingleOrDefault();
+                if (Convert.ToInt32(AvailableLeave) >= Convert.ToInt32(Eld.NoOfDays))
+                {
+                    EmployeeLeaveDetailsContext.Insert(Eld);
+                    EmployeeLeaveDetailsContext.Commit();
+                    returnText = "Success";
+                    if (returnText == "Success")
+                    {
+                        EmpLeaveBalCount(Eld);
+                        ViewBag.HeadingName = "Add Leave Type";
+                        ViewBag.HeadingColor = "bg-success";
+                        ViewBag.Msg = "Leave Apply Succesfully";
+                    }
+                }
+                else
+                {
+                    List<EmployeeDetail> EmployeeList = EmployeeDetailContext.Collection().Where(q => q.WorkingStatus == true).OrderBy(e => e.FirstName).ToList();
+                    ViewBag.EmlployeeTypeList = EmployeeList;
+                    ViewBag.Message = "Check Available Leave In Leave Type ";
+                    //return View("ApplyLeave");
+                }
             }
             return View("ApplyLeave");
         }
